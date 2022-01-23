32020af935a794386f419565e56dc6e7baa90181
Ian Boston <ieb@tfd.co.uk>
2022-01-23 10:35:21 +0000

    Testing refresh

--- transfer.sh
+++ transfer.sh
@@ -2,18 +2,18 @@
 basedir=`dirname $0`
 cd $basedir
 (
-#sudo ifup ppp0
-#sleep 5
-#ip r
-#node drive.js
+sudo ifup ppp0
+sleep 5
+ip r
+node drive.js
 git fetch
-echo sudo ifdown ppp0
+sudo ifdown ppp0
 git clean -d -f
 git status | grep "Your branch is behind 'origin/main'"
 if [ $? -eq 0 ]
 then
    git merge origin/main
-   echo pm2 restart index.js
+   pm2 restart index.js
 else
    echo "no changes"
 fi
