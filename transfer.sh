fdb952bf39781788b4e345adb8cc0a37846362b2
Ian Boston <ieb@tfd.co.uk>
2022-01-23 10:30:52 +0000

    Testing refresh

--- transfer.sh
+++ transfer.sh
@@ -2,15 +2,17 @@
 basedir=`dirname $0`
 cd $basedir
 (
-sudo ifup ppp0
-sleep 5
-ip r
-node drive.js
-command=$?
-echo Exit code was $command
-if [[ $command -eq 10 ]]
-then 
-    echo sudo shutdown -r now
+#sudo ifup ppp0
+#sleep 5
+#ip r
+#node drive.js
+git fetch
+echo sudo ifdown ppp0
+git clean -d -f
+git status | grep "Your branch is behind 'origin/main'"
+if [[ $? -eq 0 ]]
+then
+   git merge origin/main
+   echo pm2 restart index.js
 fi
-sudo ifdown ppp0
 ) 1>&2 1>> transfer.log
