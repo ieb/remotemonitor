601b51d1e98b71f9517dff5416a1f7ace5ed4d7f
Ian Boston <ieb@tfd.co.uk>
2022-01-23 11:06:32 +0000

    Improving logging

--- transfer.sh
+++ transfer.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 basedir=`dirname $0`
 cd $basedir
-gzip transfer.log
+gzip -f transfer.log
 cp transfer.log.gz data/
 (
 sudo ifup ppp0
 sleep 5
 ip r
+echo "$(date) Transfering data up"
 node drive.js
+echo "$(date) Transfer Code update"
 git fetch
 sudo ifdown ppp0
+echo "$(date) Start Code update"
 git clean -d -f
 git status | grep "Your branch is behind 'origin/main'"
 if [ $? -eq 0 ]
 then
+   echo "$(date) Code changes, restart"
    git merge origin/main
    pm2 restart index.js
 else
@@ -24,4 +28,4 @@ echo "Error Log "
 tail -100 ~/.pm2/logs/index-error.log 
 echo "Output Log "
 tail -100 ~/.pm2/logs/index-out.log 
-) 1>&2 1> transfer.log
+) 2>&1 1> transfer.log
